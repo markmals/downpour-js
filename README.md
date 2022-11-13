64e96fc81d159decba01f68b27a47cc8031a5936
Mark Malstrom <mark@malstrom.me>
2022-11-13 11:29:32 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Lib Name
+
+> Package description
+
+## Usage
+
+Install package:
+
+```sh
+# npm
+npm install lib-name
+
+# yarn
+yarn add lib-name
+
+# pnpm
+pnpm install lib-name
+```
+
+Import:
+
+```ts
+import { } from 'lib-name'
+```
+
+## License
+
+Published under [MIT License](./LICENSE).
---
+++ test/index.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from "vitest";
+import { testFunction } from "../src";
+
+describe("packageName", () => {
+    const tests = [
+        { input: "foo", output: "Hello foo" },
+        { input: "bar", output: "Hello bar" },
+    ];
+
+    for (const test of tests) {
+        it(test.input, () => {
+            expect(testFunction(test.input)).eq(test.output);
+        });
+    }
+});
